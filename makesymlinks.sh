a0738efa2cd9d3d116a9b5c5d23d0e7c40b03942
David W Millar <david.w.millar@gmail.com>
2013-02-17 06:39:15 -0500

    vim config

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@ display_status () { echo "::::: $1 :::::"; }
 # locations
 dir=~/dotfiles
 olddir=~/dotfiles_old
-files="tmux.conf tmux Xdefaults zshrc zsh cdmrc"
+files="tmux.conf tmux Xdefaults zshrc zsh cdmrc vimrc.local vimrc.bundles.local vim gitconfig"
 
 # backup
 display_status "Backing up old configs" 
@@ -24,5 +24,10 @@ for file in $files; do
   echo "  ~/.$file <- $dir/$file"
   ln -s $dir/$file ~/.$file
 done
+
+
+# spf13 (vim)
+#curl http://j.mp/spf13-vim3 -L -o - | sh
+
 
 
